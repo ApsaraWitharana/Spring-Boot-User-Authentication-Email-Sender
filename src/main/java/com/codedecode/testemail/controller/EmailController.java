2a7763477c417d4061e50e5d5aeb333ed32eb760
ApsaraWitharana <asachini095@gmail.com>
2024-09-26 12:52:01 +0530

    update-set get request mapping

--- src/main/java/com/codedecode/testemail/controller/EmailController.java
+++ src/main/java/com/codedecode/testemail/controller/EmailController.java
@@ -1,4 +1,19 @@
 package com.codedecode.testemail.controller;
 
+import com.codedecode.testemail.services.SendEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmailController {
+
+    @Autowired
+    private SendEmailService sendEmailService;
+
+    @GetMapping("sendEmail")
+    public String sendEmail(){
+        sendEmailService.sendEmail("sachiniapsara703@gmail.com","Test Body","Test Subject");
+        return "Send Successfully!!";
+    }
 }
